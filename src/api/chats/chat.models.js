09d98cadf8e9022f628a2a111b2d2b951cf4e26d
Gajanan-bhosale <gajananbhosale.official@gmail.com>
2026-01-23 12:50:10 +0530

    code pushed 1

---
+++ src/api/chats/chat.models.js
@@ -0,0 +1,68 @@
+import mongoose from 'mongoose';
+
+const chatSchema = new mongoose.Schema(
+  {
+    isGroupChat: {
+      type: Boolean,
+      default: false,
+    },
+
+    name: {
+      type: String,
+      trim: true,
+      required: function () {
+        return this.isGroupChat;
+      },
+    },
+
+    participants: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+      },
+    ],
+
+    admins: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+
+    groupCreatedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+
+    groupDescription: {
+      type: String,
+      trim: true,
+    },
+
+    groupImage: {
+      type: string,
+    },
+
+    lastMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'message',
+    },
+
+    lastMessageAt: {
+      type: Date,
+    },
+
+    isArchived: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+chatSchema.index({ participants: 1 });
+
+export default mongoose.model('Chat', chatSchema);
